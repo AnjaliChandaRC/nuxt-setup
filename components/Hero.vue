99f07fae4722c0d67aecd1bd80f05dd07cf2b6db
anjali <anjalichanda2019@gmail.com>
2024-03-26 09:44:13 +0530

    first commit

---
+++ components/Hero.vue
@@ -0,0 +1,35 @@
+<script setup>
+const list = [
+  {
+    title: "text One",
+    link: "/",
+    para: "dfghjkl",
+  },
+  {
+    title: "text Two",
+    link: null,
+    para: "dfghjkl",
+  },
+  {
+    title: "text Three",
+    link: "/two",
+    para: "dfghjkl",
+  },
+];
+</script>
+
+<template>
+  <div v-for="(obj, index) in list" :key="index">
+    <NuxtLink :to="obj.link" v-if="obj.link">{{ obj.title }} </NuxtLink>
+    <p v-else>{{ obj.title }}</p>
+    <p class="text-red-800" :class="$route.path !== '/' && 'text-green-800'">
+      {{ obj.para }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-size: 100px;
+}
+</style>
